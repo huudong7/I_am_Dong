398965f36e969ad76085bec04a770e297ed4f405
huudong7 <iamdonggg7@gmail.com>
2025-02-26 23:58:59 +0700

    tao login

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 ho_ten = input("nhập họ và tên: ")
 age =int(input("nhập tuổi của bạn alo alo: "))
-print(f"họ tên của bạn là: {ho_ten} và tuổi của bạn là tôi là đồng ahii : {age}")
+print(f"họ tên của bạn là: {ho_ten} và tuổi của bạn là : {age}")
+#tạo một login mới ở đây
